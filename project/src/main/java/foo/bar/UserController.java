b9d7129c51e147b10499c476625d5be9a9ad9bff
Fernando Felix <fernandojr.ifcg@live.com>
2016-04-14 05:13:26 -0300

    Validators.

--- project/src/main/java/foo/bar/UserController.java
+++ project/src/main/java/foo/bar/UserController.java
@@ -1,10 +1,16 @@
 package foo.bar;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+	final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+			Pattern.CASE_INSENSITIVE);
+
+	final Pattern PHONE_PATTERN = Pattern.compile("^\\([0-9]{3}\\)[0-9]{3}-[0-9]{4}$", Pattern.CASE_INSENSITIVE);
+
 	@Autowired
 	private UserRepository userRepository;
 
@@ -22,7 +33,24 @@ public class UserController {
 
 	@PostConstruct
 	public void init() {
-		this.userRepository.save(new User("Carol", "carol@troll.com", "(666) 666-6666"));
+		this.userRepository.save(new User("Carol", "carol@troll.com", "(666)666-6666"));
+	}
+
+	public boolean isNullOrEmpty(String value) {
+		return value == null || value.isEmpty();
+	}
+
+	public boolean isValidEmail(String email) {
+		return EMAIL_PATTERN.matcher(email).find();
+	}
+
+	public boolean isValidPhone(String phone) {
+		return PHONE_PATTERN.matcher(phone).find();
+	}
+
+	@ExceptionHandler
+	void handleIllegalArgumentException(IllegalArgumentException e, HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.BAD_REQUEST.value());
 	}
 
 	/**
@@ -30,7 +58,25 @@ public class UserController {
 	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public User create(String name, String email, String phone) {
-		return userRepository.save(new User(name, email, phone));
+		if (isNullOrEmpty(name) || isNullOrEmpty(email) || isNullOrEmpty(phone))
+			throw new IllegalArgumentException("The values must not be null or empty.");
+
+		if (!isValidEmail(email))
+			throw new IllegalArgumentException("The email is not correct.");
+
+		if (!isValidPhone(phone))
+			throw new IllegalArgumentException("The phone is not correct.");
+
+		User user = new User(name, email, phone);
+
+		try {
+			userRepository.save(user);
+		} catch (DataIntegrityViolationException e) {
+			throw new IllegalArgumentException("Email already registred.");
+
+		}
+
+		return user;
 	}
 
 	/**
--- project/src/main/java/foo/bar/UserRepository.java
+++ project/src/main/java/foo/bar/UserRepository.java
@@ -7,9 +7,4 @@ import org.springframework.data.repository.CrudRepository;
 @Transactional
 public interface UserRepository extends CrudRepository<User, Long> {
 
-	/**
-	 * Finds an User by its email.
-	 */
-	public User findByEmail(String email);
-
 }
