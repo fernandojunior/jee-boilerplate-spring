e6b6a293a4920b898aeed02b248c992841ba7d77
Fernando Felix <fernandojr.ifcg@live.com>
2016-04-24 13:09:11 -0300

    Minor change

--- project/src/main/webapp/js/script.js
+++ project/src/main/webapp/js/script.js
@@ -1,62 +1,51 @@
-angular.module("app", []).controller(
-		'MainController',
-		[
-				"$scope",
-				"$http",
-				"$timeout",
-				function($scope, $http, $timeout) {
+var app = angular.module("app", []);
 
-					$scope.all = function() {
-						$http.get('/project/user/').success(function(data) {
-							$scope.users = data;
-						});
-					}
+app.controller(
+	'MainController',
+	["$scope", "$http", "$timeout", function($scope, $http, $timeout) {
+		$scope.all = function() {
+			$http.get('/project/user/').success(function(data) {
+				$scope.users = data;
+			});
+		}
 
-					$scope.all();
+		$scope.all();
 
-					// Form submit handler.
-					$scope.submit = function(form) {
+		$scope.submit = function(form) {
+			$scope.submitted = true;
 
-						// Trigger validation flag.
-						$scope.submitted = true;
+			if (form.$invalid)
+				return;
 
-						// If form is invalid, return and let AngularJS
-						// show validation errors.
-						if (form.$invalid) {
-							return;
-						}
+			var context = {
+				'name' : $scope.name,
+				'email' : $scope.email,
+				'phone' : $scope.phone,
+			};
 
-						var context = {
-							'name' : $scope.name,
-							'email' : $scope.email,
-							'phone' : $scope.phone,
-						};
+			var url = '/project/user/';
 
-						var scope = $scope;
+			var res = jQuery.post(url, context, function(data, status) {
+				$scope.message = 'User has been created!';
+				$scope.all();
+				$scope.name = null;
+				$scope.email = null;
+				$scope.phone = null;
+			});
 
-						var res = jQuery.post('/project/user/', context,
-								function(data, status) {
-									console.log(data);
-									console.log(status);
-									scope.messages = 'User has been created!';
-									$scope.all();
-									$scope.name = null;
-									$scope.email = null;
-									$scope.phone = null;
-									$scope.submitted = false;
+			res.error(function(data, status) {
+				console.log(data.responseJSON.message);
+				$scope.message = data.responseJSON.message;
+			});
 
-								});
+			res.done(function() {
+				$scope.submitted = false;
+				// Hide status messages after three seconds.
+				 $timeout(function() {
+					 $scope.message = null;
+				 }, 3000);
+			});
 
-						res.fail(function(data, status) {
-							console.log(data.responseJSON.message);
-							scope.messages = data.responseJSON.message;
-						});
-						res.always(function() {
-							// Hide status messages after three seconds.
-							$timeout(function() {
-								$scope.messages = null;
-							}, 3000);
-						});
-
-					};
-				} ]);
+		};
+	}]
+);
